66841a034e16ae85cf8ffd6f8002978ff4f70af0
Pavel Zelenko <43108877+paxom4ik4@users.noreply.github.com>
2020-07-08 20:01:33 +0300

    init: final

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import './js/quiz';
+import './js/jquery.maskedinput.min';
+import './js/mail';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
+import './scss/index.scss';
